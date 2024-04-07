13a66a067f291367830cec66a387881b3ae55a96
20syl <20syl.lam78@gmail.com>
2024-04-07 21:25:00 +0200

    Create about.js

---
+++ js/about.js
@@ -0,0 +1,9 @@
+// Phrase cachée
+function showTxt() {
+    document.getElementById('txtHidden').innerHTML = ' (sauf les maths)'
+}
+
+// Reset l'affichage
+function normalTxt() {
+    document.getElementById('txtHidden').innerHTML = ''
+}
