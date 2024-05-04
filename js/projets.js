eff216d212a947f21bb9b13d5b34501217471a09
20syl <20syl.lam78@gmail.com>
2024-05-04 16:29:54 +0200

    Update projets.js
    Add API version fetching
    ==> Auto site changes

--- js/projets.js
+++ js/projets.js
@@ -11,3 +11,16 @@ function updateBar() {
         document.getElementById('bar').style.transition = '0.5s';
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {    
+    fetch('https://api.sylvain.pro/fr/versions')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('api').innerHTML = data.api;
+        document.getElementById('database').innerHTML = data.database;
+        document.getElementById('coopbot').innerHTML = data.doc_coopbot;
+        document.getElementById('coop-status').innerHTML = data.coop_status;
+        document.getElementById('coop-api').innerHTML = data.coop_api;
+        document.getElementById('nitrogen').innerHTML = data.nitrogen;
+    });
+});
