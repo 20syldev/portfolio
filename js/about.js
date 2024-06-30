2bdc3bd89f5ae40169464f4a7a0c530fb7603144
20syl <77191035+20syldev@users.noreply.github.com>
2024-06-30 19:44:30 +0200

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Temps entre 2 dates
+    try {
+        var resultat = Math.round((new Date() - new Date(2018, 7, 19)) / 31536000000);
+        document.getElementById('startDev').innerHTML = resultat;
+    }
+    catch (error) {
+    }
+});
+
 // Phrase cachée
 function showTxt() {
     document.getElementById('txtHidden').innerHTML = ' (sauf les maths)'
