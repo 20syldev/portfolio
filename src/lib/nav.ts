a8b3c9b999c78ba1d35bbc1aae8b90df95486d48
20syldev <git@sylvain.pro>
2026-01-31 16:15:23 +0100

    MINOR: lib: Add page titles to navigation config

--- src/lib/nav.ts
+++ src/lib/nav.ts
@@ -1,2 +1,7 @@
 export const tabs = ["Accueil", "Alternance", "Veille"];
 export const urls = ["/", "/alternance/", "/veille/"];
+export const titles = [
+    "Sylvain L. - Développeur Full Stack",
+    "Alternance - Sylvain L.",
+    "Veilles Technologiques - Sylvain L.",
+];
