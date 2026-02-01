053ab92218f258b4bfbb0c173c11eb68c50df286
20syldev <git@sylvain.pro>
2026-02-01 13:01:52 +0100

    MINOR: data: Extract technologies data from component

---
+++ src/data/technologies.ts
@@ -0,0 +1,121 @@
+/** Icons that need to be inverted in dark mode (black icons) */
+export const darkInvertIcons = [
+    "Rust",
+    "Bash",
+    "Next.js",
+    "VitePress",
+    "GitHub",
+    "Adobe",
+    "Cinema 4D",
+    "Markdown",
+];
+
+/** Icons that need invert + hue-rotate to preserve colors (black + colored) */
+export const darkInvertHueIcons = ["YAML"];
+
+/** Technologies organized by category */
+export const techCategories = [
+    {
+        name: "Langages",
+        items: [
+            { name: "HTML", icon: "/icons/tech/html.svg" },
+            { name: "CSS", icon: "/icons/tech/css.svg" },
+            { name: "JavaScript", icon: "/icons/tech/javascript.svg" },
+            { name: "TypeScript", icon: "/icons/tech/typescript.svg" },
+            { name: "PHP", icon: "/icons/tech/php.svg" },
+            { name: "Python", icon: "/icons/tech/python.svg" },
+            { name: "Java", icon: "/icons/tech/java.svg" },
+            { name: "C", icon: "/icons/tech/c.svg" },
+            { name: "C++", icon: "/icons/tech/cplusplus.svg" },
+            { name: "C#", icon: "/icons/tech/csharp.svg" },
+            { name: "Go", icon: "/icons/tech/go.svg" },
+            { name: "Rust", icon: "/icons/tech/rust.svg" },
+            { name: "Bash", icon: "/icons/tech/bash.svg" },
+            { name: "Ruby", icon: "/icons/tech/ruby.svg" },
+            { name: "Swift", icon: "/icons/tech/swift.svg" },
+            { name: "Kotlin", icon: "/icons/tech/kotlin.svg" },
+            { name: "Dart", icon: "/icons/tech/dart.svg" },
+            { name: "Perl", icon: "/icons/tech/perl.svg" },
+        ],
+    },
+    {
+        name: "Frameworks",
+        items: [
+            { name: "React", icon: "/icons/tech/react.svg" },
+            { name: "Next.js", icon: "/icons/tech/nextjs.svg" },
+            { name: "Node.js", icon: "/icons/tech/nodejs.svg" },
+            { name: "Tailwind CSS", icon: "/icons/tech/tailwindcss.svg" },
+            { name: "Bootstrap", icon: "/icons/tech/bootstrap.svg" },
+            { name: "Vue.js", icon: "/icons/tech/vuejs.svg" },
+            { name: "Flutter", icon: "/icons/tech/flutter.svg" },
+            { name: "Vite.js", icon: "/icons/tech/vitejs.svg" },
+            { name: "jQuery", icon: "/icons/tech/jquery.svg" },
+            { name: "React Native", icon: "/icons/tech/reactnative.svg" },
+            { name: "Vuetify", icon: "/icons/tech/vuetify.svg" },
+            { name: "VitePress", icon: "/icons/tech/vitepress.svg" },
+        ],
+    },
+    {
+        name: "Bases de données",
+        items: [
+            { name: "MongoDB", icon: "/icons/tech/mongodb.svg" },
+            { name: "PostgreSQL", icon: "/icons/tech/postgresql.svg" },
+            { name: "MySQL", icon: "/icons/tech/mysql.svg" },
+            { name: "SQLite", icon: "/icons/tech/sqlite.svg" },
+            { name: "Firebase", icon: "/icons/tech/firebase.svg" },
+            { name: "Elasticsearch", icon: "/icons/tech/elasticsearch.svg" },
+        ],
+    },
+    {
+        name: "Outils & IDE",
+        items: [
+            { name: "VS Code", icon: "/icons/tech/vscode.svg" },
+            { name: "Vim", icon: "/icons/tech/vim.svg" },
+            { name: "Android Studio", icon: "/icons/tech/androidstudio.svg" },
+            { name: "npm", icon: "/icons/tech/npm.svg" },
+            { name: "Nodemon", icon: "/icons/tech/nodemon.svg" },
+            { name: "PostCSS", icon: "/icons/tech/postcss.svg" },
+            { name: "tmux", icon: "/icons/tech/tmux.svg" },
+        ],
+    },
+    {
+        name: "Formats",
+        items: [
+            { name: "JSON", icon: "/icons/tech/json.svg" },
+            { name: "YAML", icon: "/icons/tech/yaml.svg" },
+            { name: "Markdown", icon: "/icons/tech/markdown.svg" },
+        ],
+    },
+    {
+        name: "Systèmes",
+        items: [
+            { name: "Linux", icon: "/icons/tech/linux.svg" },
+            { name: "Linux Mint", icon: "/icons/tech/linuxmint.svg" },
+            { name: "Fedora", icon: "/icons/tech/fedora.svg" },
+            { name: "Windows", icon: "/icons/tech/windows.svg" },
+            { name: "Raspberry Pi", icon: "/icons/tech/raspberrypi.svg" },
+            { name: "Android", icon: "/icons/tech/android.svg" },
+        ],
+    },
+    {
+        name: "DevOps & Infra",
+        items: [
+            { name: "Git", icon: "/icons/tech/git.svg" },
+            { name: "GitHub", icon: "/icons/tech/github.svg" },
+            { name: "Docker", icon: "/icons/tech/docker.svg" },
+            { name: "Nginx", icon: "/icons/tech/nginx.svg" },
+            { name: "Apache", icon: "/icons/tech/apache.svg" },
+            { name: "Kibana", icon: "/icons/tech/kibana.svg" },
+        ],
+    },
+    {
+        name: "Design & 3D",
+        items: [
+            { name: "Godot", icon: "/icons/tech/godot.svg" },
+            { name: "Adobe", icon: "/icons/tech/adobe.svg" },
+            { name: "Figma", icon: "/icons/tech/figma.svg" },
+            { name: "Blender", icon: "/icons/tech/blender.svg" },
+            { name: "Cinema 4D", icon: "/icons/tech/cinema4d.svg" },
+        ],
+    },
+];
